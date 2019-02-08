c4a7db8fecc9dd8e0add95908c8a71d139d4dfb8
Finite <33845166+finia2NA@users.noreply.github.com>
2019-02-08 14:34:32 +0100

    sadf

---
+++ getter.py
@@ -0,0 +1,34 @@
+# imports
+import urllib.request
+import urllib.parse
+import re
+import os
+
+
+def getUrl(searchString):
+    query_string = urllib.parse.urlencode({"search_query": searchString})
+    html_content = urllib.request.urlopen(
+        "http://www.youtube.com/results?" + query_string
+    )
+    search_results = re.findall(
+        r"href=\"\/watch\?v=(.{11})", html_content.read().decode()
+    )
+    return "http://www.youtube.com/watch?v=" + search_results[0]
+
+
+def download(url):
+    os.system(
+        "youtube-dl.exe "
+        + url
+        + "-x, --extract-audio "
+        + "--audio-format mp3 "
+        + "--audio-quality 0 "
+        + "-f bestaudio"
+    )
+
+
+def test():
+    download(getUrl("be right there boombox cartel remix"))
+
+
+test()
