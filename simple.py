5ad05d2c89b248ed643ffea15f7f11acbfef4fac
Finite <33845166+finia2NA@users.noreply.github.com>
2019-02-12 17:09:16 +0100

    update simple.py to new workflow
    with different rendering/export locations

--- simple.py
+++ simple.py
@@ -1,20 +1,37 @@
 import sys
 import youtube_dl
+import os
+import shutil
 
-def getLocation():
-    return open ("destination.txt", "r").read()
+
+def getDestination():
+    return open("destination.txt", "r").read()
+
+
+def getRenderLocation():
+    return open("renderlocation.txt", "r").read()
+
 
 ydl_opts = {
     "format": "bestaudio/best",
     "postprocessors": [
         {
             "key": "FFmpegExtractAudio",
-            "preferredcodec": "mp3",
+            "preferredcodec": "wav",
             "preferredquality": "192",
         }
     ],
-    "outtmpl": getLocation() + "/%(title)s.%(ext)s",
+    "outtmpl": getRenderLocation() + "/%(title)s.%(ext)s",
 }
+
+
+def movefiles():
+    source = getRenderLocation()
+    dest = getDestination()
+
+    files = os.listdir(source)
+    for f in files:
+        shutil.move(source + f, dest)
 
 
 url = sys.argv[1]
@@ -23,3 +40,5 @@ print(url)
 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download([url])
+
+movefiles()
