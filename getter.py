a4aea4b1c270dbd5abe55ce9bba7e8f953445dac
Richard Hanß <33845166+finia2NA@users.noreply.github.com>
2019-12-15 18:45:19 +0100

    refactor a bit

--- getter.py
+++ getter.py
@@ -9,7 +9,7 @@ import youtube_dl
 import core
 
 
-def getUrl(searchString) -> str:
+def search(searchString) -> str:
   query_string = urllib.parse.urlencode({"search_query": searchString})
   html_content = urllib.request.urlopen(
       "http://www.youtube.com/results?" + query_string
@@ -20,7 +20,7 @@ def getUrl(searchString) -> str:
   return "http://www.youtube.com/watch?v=" + search_results[0]
 
 
-if __name__ == "__main__":
+def main():
   # print(len(sys.argv))
   # for i in range(0, len(sys.argv)):
   #     print(sys.argv[i])
@@ -33,7 +33,10 @@ if __name__ == "__main__":
   print("searching for: " + input)
   # print("found " + getAuthorAndName(url))
 
-  url = getUrl(input)
-  core.download(url)
+  core.download(search(input))
 
   print("finished")
+
+
+if __name__ == "__main__":
+  main()
