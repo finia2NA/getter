fa1b9937906e5bf4a3d362916bb6b48c195d09df
Richard Hanß <33845166+finia2NA@users.noreply.github.com>
2019-12-15 21:55:28 +0100

    wip visual

--- visual/visual.py
+++ visual/visual.py
@@ -5,11 +5,12 @@
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit, QComboBox
 
 app = QApplication([])
-# app.setApplicationName("Getter Visual")
+app.setApplicationName("Getter Visual")
 
 window = QWidget()
 main_layout = QVBoxLayout()
 window.setLayout(main_layout)
+
 
 class quickWidget(QWidget):
   def __init__(self):
@@ -21,29 +22,44 @@ class quickWidget(QWidget):
     quickLayout.addWidget(video_button)
     quickLayout.addWidget(itunes_button)
 
+
 class pathWidget(QWidget):
   def __init__(self):
     QWidget.__init__(self)
     pathLayout = QHBoxLayout()
     self.setLayout(pathLayout)
-    pathLocation = QLineEdit()
-    pathSelectButton = QPushButton("select")
-    pathLayout.addWidget(pathLocation)
-    pathLayout.addWidget(pathSelectButton)
+    self.pathLocation = QLineEdit()
+    self.pathSelectButton = QPushButton("select")
+    pathLayout.addWidget(self.pathLocation)
+    pathLayout.addWidget(self.pathSelectButton)
+
+  def getPath(self):
+    self.pathLocation.text()
+
+  def setPath(self, newPath: str):
+    self.pathLocation.setText(newPath)
+
 
 class downloadWidget(QWidget):
   def __init__(self):
     QWidget.__init__(self)
     downloadLayout = QHBoxLayout()
     self.setLayout(downloadLayout)
-    formatSelector = QComboBox()
-    downloadButton = QPushButton("download")
-    downloadLayout.addWidget(formatSelector)
-    downloadLayout.addWidget(downloadButton)
+    self.formatSelector = QComboBox()
+    formats = ["wav", "mp3", "mp4"]
+    self.formatSelector.addItems(formats)
+    self.downloadButton = QPushButton("download")
+    downloadLayout.addWidget(self.formatSelector)
+    downloadLayout.addWidget(self.downloadButton)
+
+  def getFormat(self):
+    return self.formatSelector.currentText()
+
 
 main_layout.addWidget(quickWidget())
 main_layout.addWidget(pathWidget())
 main_layout.addWidget(downloadWidget())
+
 
 window.show()
 app.exec_()
